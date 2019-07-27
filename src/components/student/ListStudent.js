68d990953f63125fd3a5ca285e7887349a54ac3b
mdquilindo <mdquilindo@unicauca.edu.co>
2019-07-27 17:15:59 -0500

    upload

--- src/components/student/ListStudent.js
+++ src/components/student/ListStudent.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
-import { Button, Modal, Table } from 'react-bootstrap';
-// import axios from 'axios';
+import { Button, Modal, Table, Form, FormControl } from 'react-bootstrap';
+import axios from 'axios';
 import AddStudent from './AddStudent';
 
 export default class ListStudent extends Component {
 	constructor(props, context) {
 		super(props, context);
 		this.state = {
-			show: false
+			cvs_file: '',
+			show: false,
+			showUpload: false
 		};
-		this.handleClose = this.handleClose.bind(this);
-		this.handleShow = this.handleShow.bind(this);
 	}
 
-	handleClose() {
-		this.setState({ show: false });
+	handleChange = (event) => {
+		console.log(event)
+		const target = event.target;
+		const value = target.type === "file" ? target.files : target.value;
+		const name = target.name;
+		this.setState({
+			[name]: value
+		});
 	}
 
-	handleShow() {
+	handleShow = () => {
 		this.setState({ show: true });
+	}
+
+	handleUpload = () => {
+		this.setState({ showUpload: true });
+	}
+
+	subir = () => {
+		console.log(this.state.cvs_file)
+		this.setState({ showAlert: false });
+		var json = {
+			"cvs_file": this.state.cvs_file
+		}
+		let form_data = new FormData();
+		form_data.append('csv_file', this.state.cvs_file[0], this.state.cvs_file[0].name);
+		form_data.append('title', 'file');
+		form_data.append('content', 'csv');
+		axios.post("http://localhost:8000/api/file/", form_data, {
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		})
+			.then(() => {
+				// this.loadStudents()
+			})
 	}
 
 	render() {
@@ -26,7 +56,7 @@ export default class ListStudent extends Component {
 			<>
 				<div className="title pt-4 mb-2">
 					<h4 className="d-inline white">Gestionar estudiantes</h4>
-					<Button className="d-inline float-right btn btn-light mb-2 ml-2 subir"></Button>
+					<Button className="d-inline float-right btn btn-light mb-2 ml-2 subir" onClick={this.handleUpload}></Button>
 					<Button className="d-inline float-right btn btn-light mb-2 agregar" onClick={this.handleShow}></Button>
 				</div>
 				<Table striped bordered hover responsive="xl" size="xl">
@@ -62,6 +92,21 @@ export default class ListStudent extends Component {
 					</Modal.Body>
 					<Modal.Footer>
 						<Button variant="secondary" onClick={this.handleClose}>Cerrar</Button>
+					</Modal.Footer>
+				</Modal>
+				{/* Upload students */}
+				<Modal show={this.state.showUpload} onHide={this.handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>Cargar reporte estudiantes</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Form>
+							<FormControl name="cvs_file" type="file" ref={this.state.file} onChange={this.handleChange}></FormControl>
+						</Form>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={this.handleClose}>Cancelar</Button>
+						<Button variant="primary" name="eliminar" onClick={this.subir}>Subir</Button>
 					</Modal.Footer>
 				</Modal>
 			</>
