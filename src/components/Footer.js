c13439f6976e9cbf950c2e7e269fd2f8b5573306
Dardila11 <danieleardila@hotmail.com>
2019-08-05 12:46:49 -0500

    pequeñas modificaciones

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default class Footer extends Component {
             <div className="container-footer">
                     <footer className="d-flex row footer">
                         <div className="seccion-logo col-md-2">
-                            <a href="http://www.unicauca.edu.co/versionP/" target="_blank">
+                            <a href="https://www.unicauca.edu.co/versionP/" target="_blank">
                                 <div className="logo"></div>
                             </a>
                         </div>
@@ -18,17 +18,17 @@ export default class Footer extends Component {
                             <a href="/politicas/" target="_blank"><span>Politcas de seguridad</span></a>
                         </div>
                         <div className="seccion-footer pt-3 col-md-2">
-                            <span className="negrilla d-block">Sobre nostros</span>
+                            <span className="negrilla d-block">Sobre nosotros</span>
                             <span className="d-block">Fotos</span>
                         </div>
                         <div className="seccion-footer pt-3 col-md-2">
                             <span className="negrilla">Enlaces</span>
-                            <a href=""><div className="facebook d-block"></div></a>
+                            <a href="https://www.facebook.com/FIET-Facultad-de-Ingenier%C3%ADa-Electrónica-y-Telecomunicaciones-Unicauca-118309118807869/" target="_blank"><div className="facebook d-block"></div></a>
                         </div>
                         <div className="seccion-footer pt-3 col-md-4">
                             <span className="negrilla">Contacto</span>
                             <span className="d-block"><FontAwesomeIcon icon={faPhoneAlt} /> (2) 8209800</span>
-                            <span className="d-block"><FontAwesomeIcon icon={faMapMarkedAlt} /> Cra. 2 ##4N-140, Popayán, Cauca</span>
+                            <span className="d-block"><FontAwesomeIcon icon={faMapMarkedAlt} /> Cra. 2 #4N-140, Popayán, Cauca</span>
                             <span className="d-block"><FontAwesomeIcon icon={faHandPointRight} /> Facultad de Ingenieria Electronica y Telecomunicaciones - FIET</span>
                         </div>
                     </footer>
